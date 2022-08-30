77c01a7d74f339626685c3a87b296b1ab5ea4fef
NA <nadira.alisovna@gmail.com>
2022-08-30 16:57:38 +0300

    задача 2

--- 3.2.py
+++ 3.2.py
@@ -1,25 +1,6 @@
-# # задача 2
-#
-# name,surname,year,city,email,number=input('введите имя: '),input('введите фамилию: '),input('введите год рождения: '),input('введите город: '),input('введите email: '),input('введите номер телефона: ')
-def func_data(name,surname,year,city,email,number):
-    print(name,surname,year,city,email,number)
-func_data('Надира,','Ахметова,',2000,'Санкт-Петербург','sill.a@mail.ri','+78126164535')
+# задача 2
 
-# # сделать словарем
-#
-# # def my_func(**kparams):
-# #     return kparams
-# #
-# # print(my_func(el_1=10, el_2=20, el_3="text"))
-#
-# # или
-# # def my_func(v2,v1,v3):
-# #     print(f'v2-{v2},v3-{v3},v1-{v1}')
-# # my_func(v1=10,v2=11,v3=13)
-
-# или
-# # def my_func(**kwargs):
-# #     return kwargs
-# # print(my_func(el_1=10, el_2=20, el_3="text"))
-# # Результат:
-# # {'el_1': 10, 'el_2': 20, 'el_3': 'text'}
+name,surname,year,city,email,number=input('введите имя: '),input('введите фамилию: '),input('введите год рождения: '),input('введите город: '),input('введите email: '),input('введите номер телефона: ')
+def func_data(**arg):
+    return arg
+print(func_data(name=name,surname=surname,year=year,city=city,email=email,number=number))
